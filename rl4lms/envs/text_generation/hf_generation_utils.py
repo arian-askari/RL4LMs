302bc2f04e82f8dea97603a425afe0c5f209a968
Arian Askari <diyardownload@gmail.com>
2023-06-12 04:35:55 +0200

    Update hf_generation_utils.py

--- rl4lms/envs/text_generation/hf_generation_utils.py
+++ rl4lms/envs/text_generation/hf_generation_utils.py
@@ -24,9 +24,11 @@ import torch
 import torch.distributed as dist
 from torch import nn
 
-from transformers.generation_beam_constraints import Constraint, DisjunctiveConstraint, PhrasalConstraint
-from transformers.generation_beam_search import BeamScorer, BeamSearchScorer, ConstrainedBeamSearchScorer
-from transformers.generation_logits_process import (
+
+from transformers.generation.beam_constraints import Constraint, DisjunctiveConstraint, PhrasalConstraint
+
+from transformers.generation.beam_search import BeamScorer, BeamSearchScorer, ConstrainedBeamSearchScorer
+from transformers.generation.logits_process import (
     EncoderNoRepeatNGramLogitsProcessor,
     ExponentialDecayLengthPenalty,
     ForcedBOSTokenLogitsProcessor,
@@ -44,13 +46,14 @@ from transformers.generation_logits_process import (
     TopPLogitsWarper,
     TypicalLogitsWarper,
 )
-from transformers.generation_stopping_criteria import (
+from transformers.generation.stopping_criteria import (
     MaxLengthCriteria,
     MaxTimeCriteria,
     StoppingCriteria,
     StoppingCriteriaList,
     validate_stopping_criteria,
 )
+
 from transformers.pytorch_utils import torch_int_div
 from transformers.utils import ModelOutput, logging
 
